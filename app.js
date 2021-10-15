e14c45f55b1a617283c866f1f0a0fd8b47f0fbdf
Geiner Lara <davidlararada1062@hotmail.com>
2021-10-14 23:00:51 -0500

    se realizo el back del login

--- app.js
+++ app.js
@@ -1,48 +1,45 @@
-const express = require("express")
-const cors = require("cors")
-const app = express()
-const router = require('./routes')
-const port = process.env.PORT || 4000
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+const router = require("./routes");
+const port = process.env.PORT || 4000;
 
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 
-// Connect MongoDB 
-require('./connection')
+// Connect MongoDB
+require("./connection");
 
+// app.use(bodyParser.json());
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: false,
+//   })
+// );
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
 
-// app.use(bodyParser.json()); 
-// app.use( 
-//   bodyParser.urlencoded({ 
-//     extended: false, 
-//   }) 
-// ); 
-app.use(cors())
-app.use(express.json())
-
- 
 // API routes
-app.use("/api", router)
-
+app.use("/api", router);
 
 // Static files route
-app.use(express.static('./public'));
- 
+app.use(express.static("./public"));
 
 // APP STARTER
-app.listen(port, () => { 
-  console.log("Connected to port " + port)
-})
- 
+app.listen(port, () => {
+  console.log("Connected to port " + port);
+});
 
-// Find 404 
-app.use((req, res, next) => { 
+// Find 404
+app.use((req, res, next) => {
   // next(createError(404))
-  res.send('Error 404')
-})
- 
-// error handler 
-// app.use(function(err, req, res) { 
-//   console.error(err.message); 
-//   if (!err.statusCode) err.statusCode = 500; 
-//   res.status(err.statusCode).send(err.message) 
+  res.send("Error 404");
+});
+
+// error handler
+// app.use(function(err, req, res) {
+//   console.error(err.message);
+//   if (!err.statusCode) err.statusCode = 500;
+//   res.status(err.statusCode).send(err.message)
 // })
